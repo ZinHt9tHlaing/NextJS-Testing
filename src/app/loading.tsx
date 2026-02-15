47d39c57c0aad1cbb8aee8b06548a4d50552c10c
ZinHt9tHlaing <phyoearkar267@gmail.com>
2026-02-15 23:34:13 +0630

    loading ui

---
+++ src/app/loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loading = async () => {
+  return (
+    <div>
+      <h2 className="text-center text-lg font-medium text-red-400 animate-pulse">
+        App is loading...
+      </h2>
+    </div>
+  );
+};
+
+export default Loading;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div>Hello World</div>;
+export default async function Home() {
+  const data = await new Promise((resolve) =>
+    setTimeout(() => resolve("App is loaded"), 2000),
+  );
+
+  return <div>{data as string}</div>;
 }
